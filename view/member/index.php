b3bef0b9d984515636e6afcb1f3e2d365f2ce176
[{웹개발4팀}] {박은별} <{hellostella226@gmail.com}>
2018-11-15 17:10:58 +0900

    dev source 18/11/15 17:09
    1차 검토, 요구사항에 맞게 재수정 필요
    1: index.php를 통하게 경로 재수정

---
+++ view/member/index.php
@@ -0,0 +1,24 @@
+<?php
+
+if(empty($_GET[mode])){
+
+    echo"<script>alert('잘못된 접근입니다.'); location.href='/'; </script>";
+    exit;
+
+}
+
+switch ($_GET[mode]){
+
+    case 'step_01' : $fileName = "/view/member/step_01.html"; break;
+    case 'step_02' : $fileName = "/view/member/step_02.html"; break;
+    case 'step_03' : $fileName = "/view/member/step_03.html"; break;
+    case 'regist' : $fileName = "/model/regist.php"; break;
+    case 'complete' : $fileName = "/view/member/complete.html"; break;
+    case 'find_id' : $fileName = "/view/member/findId.html"; break;
+    case 'find_pass' : $fileName = "/view/member/findPassword.html"; break;
+    case 'modify' : $fileName = "/view/member/modify.html"; break;
+
+}
+
+if($fileName) include_once $_SERVER['DOCUMENT_ROOT'].$fileName ;
+
